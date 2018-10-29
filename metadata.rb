7a4b2df7dbedf4405667b1fe8dcaf10be128378d
Adam Feuer <adam.feuer@twistle.com>
2018-10-29 16:31:58 -0700

    added metadata.rb
    - so we can install from git

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name             'poise-python'
+maintainer       'poise'
+maintainer_email 'noah@coderanger.net'
+license          'Apache-2.0'
+description      'Installs Python'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.7.1.pre'
+
+%w(amazon centos debian fedora oracle redhat scientific ubuntu).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/poise/poise-python'
+issues_url 'https://github.com/poise/poise-python/issues'
+chef_version '>= 12.1'
