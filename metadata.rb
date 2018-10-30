0bb1885fa41bde8dc989e05d6ed44a0052814c7a
Adam Feuer <adam.feuer@twistle.com>
2018-10-29 17:17:49 -0700

    making metadata.rb match metadata.json
    - from released version

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,17 @@
 name             'poise-python'
-maintainer       'poise'
+maintainer       'Noah Kantrowitz'
 maintainer_email 'noah@coderanger.net'
 license          'Apache-2.0'
 description      'Installs Python'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.7.1'
 
-%w(amazon centos debian fedora oracle redhat scientific ubuntu).each do |os|
+%w(amazon centos debian fedora redhat ubuntu windows).each do |os|
   supports os
 end
 
+depends 'poise', '~> 2.7',
+depends 'poise-languages', '~> 2.0'
 source_url 'https://github.com/poise/poise-python'
 issues_url 'https://github.com/poise/poise-python/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.14', '< 15'
